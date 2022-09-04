f26ceb4d1af4779cc0b478a96cab051605b21c85
Ahmad <Kingahmed19991@gmail.com>
2022-09-04 13:18:28 +0300

    finished the dragging logic

--- src/App.js
+++ src/App.js
@@ -1,39 +1,76 @@
 import SunImage from './images/icon-sun.svg'
 import MoonImage from './images/icon-moon.svg'
 import TodoItem from './todo-item'
-import {useState} from 'react'
+import {useState, useEffect} from 'react'
+import {DragDropContext, Droppable, Draggable} from 'react-beautiful-dnd'
 
 function App() {
   const [nightMode, setNightMode] = useState(false)
   const [todos, setTodos] = useState([])
-  var filteredTodos = todos
-  var parsedTodos = filteredTodos.map((todo, i)=>{
+  const [viewedTodos, setViewedTodos] = useState(todos)
+
+useEffect(()=>{
+  setViewedTodos(todos)
+},[todos])
+
+
+  var parsedTodos = viewedTodos.map((todo, i)=>{
     return (
-      <TodoItem todo={todo} key={i} index={i} todos={todos} setTodos={setTodos} />
+      <Draggable key={i} draggableId={i.toString()} index={i}>
+  {(provided, snapshot) => (
+    <div
+      ref={provided.innerRef}
+      {...provided.draggableProps}
+      {...provided.dragHandleProps}
+    >
+        <TodoItem todo={todo} index={i} todos={todos} setTodos={setTodos}/>
+    </div>
+  )}
+</Draggable>
     )
   })
   function addTodo(ev){
     if(ev.key === 'Enter'){
       setTodos(
         [...todos,{value:ev.target.value,checked:false}]
-      )
-      ev.target.value = ''
+        )
+        ev.target.value = ''
+        console.log(todos);
     }
   }
   function allBtn(){
-    filteredTodos = todos
+    setViewedTodos(todos)
   }
   function completedBtn(){
-    filteredTodos = todos.filter((value,index,array)=>{
-      return value.checked === true
-    })
+    setViewedTodos(
+      todos.filter(val=>{
+        return val.checked
+      })
+    )
   }
   function activeBtn(){
-    filteredTodos = todos.filter((value,index,array)=>{
-      return value.checked !== true
-    })
+    setViewedTodos(
+      todos.filter(val=>{
+        return !val.checked
+      })
+    )
   }
-  
+  function clearCompleted(){
+    setTodos(
+      todos.filter(val=>{
+        return !val.checked
+      })
+    )
+  }
+  function dragEnd(ev){
+    var dest = ev.destination.index
+    var src = ev.source.index
+    var filtered = todos.filter((val,index)=>{
+      return index !== src
+    })
+    setTodos([...filtered.slice(0, dest),{value:todos[src].value,checked:todos[src].checked},...filtered.slice(dest)])
+  }
+
   return (
     <div className={nightMode ? 'dark' : 'light'}>
       <div className='container'>
@@ -55,22 +92,34 @@ function App() {
 
 
   <section className="todo-body-footer">
-      <div className="todo-body">
-        {
-          [...parsedTodos]
-        }
-      </div>
+    <DragDropContext onDragEnd={dragEnd}>
+        <div className="todo-body">
+      <Droppable droppableId='hola' type="PERSON">
+  {(provided, snapshot) => (
+    <div
+      ref={provided.innerRef}
+      {...provided.droppableProps}
+    >
+            {[...parsedTodos]}
+      {provided.placeholder}
+      
+    </div>
+  )}
+  
+</Droppable>;
+  </div>
+    </DragDropContext>
 
       <div className="footer">
         <div className="items-left">
-          5 items left
+          {todos.length} items left
         </div>
         <div className="filter">
-          <p onClick={allBtn}>All</p>
-          <p onClick={activeBtn}>Active</p>
-          <p onClick={completedBtn}>Completed</p>
+          <p tabIndex={0} onClick={allBtn}>All</p>
+          <p tabIndex={0} onClick={activeBtn}>Active</p>
+          <p tabIndex={0} onClick={completedBtn}>Completed</p>
         </div>
-        <div className="clear-completed" onClick={()=>{setTodos([])}}>
+        <div className="clear-completed" onClick={clearCompleted}>
           Clear Completed
         </div>
       </div>
--- src/todo-item.js
+++ src/todo-item.js
@@ -1,4 +1,5 @@
 import Cross from './images/icon-cross.svg'
+import CheckImage from './images/icon-check.svg'
 
 function TodoItem(props){
     function removeItem(){
@@ -9,16 +10,34 @@ function TodoItem(props){
         )
     }
     function checkTodo(){
+        var todo = props.todo.value
+        var ind = props.index
+        var f = props.todos.filter((value,index,array)=>{
+            return index !== props.index
+        })
             props.setTodos(
-                [...props.todos.filter((value,index,array)=>{
-                    return index !== props.index
-                }),{value:props.todo.value,checked:!props.todo.checked}]
+                [...f.slice(0, ind),{value:todo,checked:true},...f.slice(ind)]
             )
     }
-
+    function drag(ev){
+        var bodyY = document.querySelector('.todo-body').getBoundingClientRect().top
+        var elPos = ev.target.getBoundingClientRect()
+        console.log(elPos, bodyY);
+    }
+    function changeOrder(ind){
+        var todo = props.todo.value
+        var f = props.todos.filter((value,index,array)=>{
+            return index !== props.index
+        })
+            props.setTodos(
+                [...f.slice(0, ind),{value:todo,checked:true},...f.slice(ind)]
+            )
+    }
     return (
-        <div className="todo-item">
-            <div className={props.todo.checked ? 'checked' : 'circle'} onClick={checkTodo}></div>
+        <div className="todo-item" draggable="true" onDrag={drag}>
+            <div className={props.todo.checked ? 'checked' : 'circle'} onClick={checkTodo}>
+                <img src={CheckImage} alt="check sign" />
+            </div>
             <div className="todo">{props.todo.value}</div>
             <img src={Cross} 
             alt="cross todo" 
